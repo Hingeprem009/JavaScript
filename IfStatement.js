0db5153f2439e46caa185fb4a21353adb1d49fc1
Hingeprem009 <hingeprem6447@gmail.com>
2026-06-24 12:34:09 +0530

    added else if block

--- IfStatement.js
+++ IfStatement.js
@@ -11,7 +11,11 @@ else
 let age = 17;
 if(age >= 18)
 {
-    console.log("You can drive!");
+    console.log('You can drive!');
+}
+else if(age == 17)
+{
+    console.log('Almost there!');
 }
 else
 {
