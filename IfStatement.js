6b7b51763a1c198b6b3bf9619daf4b35b1a89210
Hingeprem009 <hingeprem6447@gmail.com>
2026-06-24 12:26:35 +0530

    Learned about booleans and conditional statements

---
+++ IfStatement.js
@@ -0,0 +1,19 @@
+if(true)
+{
+    console.log('hello');
+}
+else
+{
+    console.log('else');
+}
+
+// Check if the person is old enough to drive
+let age = 18;
+if(age >= 19)
+{
+    console.log("You can drive!");
+}
+else
+{
+    console.log("You can't drive!");
+}
